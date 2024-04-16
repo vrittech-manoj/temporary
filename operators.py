026ef5596a5325c2353130be536d3d06599a1609
vrittech-manoj <manoj@vrittechnologies.com>
2024-04-16 17:56:36 +0545

    operator

---
+++ operators.py
@@ -0,0 +1,12 @@
+# print(3989+3298)
+a = 450
+b = 890
+print(a+b)
+print(a-b) #it subtract
+print(a*b) #it multiplly
+
+#operator 
+# +,-,*,/,//,%,>,<,=,==,!=
+# and , or , 
+
+
---
+++ variables.py
@@ -0,0 +1,10 @@
+
+name = "manoj"
+cast = "das" 
+
+#kjahsdiuqwheuh
+
+number =  230 #integer
+number_1 = "230iusydiuuayeuqwo" #string
+
+print(type(number)) #it checkes variable types
