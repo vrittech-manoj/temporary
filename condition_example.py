95f5cd799ffc7e82059b00aefc942e0cb0cee720
vrittech-manoj <manoj@vrittechnologies.com>
2024-04-17 16:14:24 +0545

    condition

---
+++ condition_example.py
@@ -0,0 +1,14 @@
+#WAP to check a given number is less than 10 .
+#WAP to check a given number is less than 10 or greater.
+#WAP to check a given number is less than 10 or greater or equal to.
+
+
+#condition,loops,function,class.. space inden..,(:)
+
+# name = "ram"
+# if name == "ram":
+#     print("he is male")
+# else:
+#     print(" she is female")
+print(type("asd"))
+print(str(type("hsasa")) == "<class 'str'>")
---
+++ task.py
@@ -0,0 +1,21 @@
+def isPure(value):
+    try:
+        int(value)
+        return True
+    except:
+        return False
+    
+value_str = "kjslksdj"
+check = isPure(value_str)
+if check == True:
+    print(" it is integer")
+else:
+    print("it is string")
+#WAP to take age from people , if  age <18 then print "you can not vote" if age is greter than 18 print "you can vote"
+#WAP to print below format
+    # <12 :"you are child hood"
+    # >12-<20 "you are teen agers"
+    # >20-<50 "you are adult"
+    # >50 print "you are old"
+
+
