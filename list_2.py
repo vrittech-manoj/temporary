4544ca4e4ddb76142779e1f993b191a66244d05f
vrittech-manoj <manoj@vrittechnologies.com>
2024-04-30 16:44:41 +0545

    list dict

---
+++ list_2.py
@@ -0,0 +1,15 @@
+# def doNothingwithValue(list_1):
+#     list_1.append(89)
+
+# a = ["ball","cat"]
+# print(a)
+# doNothingwithValue(a)
+# print(a)
+
+def doNothingwithValue(value):
+    return 897
+
+a = 9
+a = doNothingwithValue(a)
+print(a)
+
